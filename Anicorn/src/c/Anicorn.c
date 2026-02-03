e666ed962eb726430a2a16ac468651f11b15ce59
PacoMolino <ahogadoenlared@gmail.com>
2026-02-03 15:36:57 +0000

    fetch api example

--- Anicorn/src/c/Anicorn.c
+++ Anicorn/src/c/Anicorn.c
@@ -1,60 +1,85 @@
 #include <pebble.h>
 
-static Window *s_window;
-static TextLayer *s_text_layer;
+static Window *s_main_window;
+static TextLayer *s_status_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+// --- COMUNICACIÓN APP MESSAGE ---
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Se ejecuta cuando el JS nos envía datos
+  Tuple *data_tuple = dict_find(iterator, MESSAGE_KEY_DATA_KEY);
+  
+  if(data_tuple) {
+    text_layer_set_text(s_status_layer, data_tuple->value->cstring);
+  }
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  text_layer_set_text(s_status_layer, "Error de envío");
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
+// --- LÓGICA DE BOTONES ---
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_status_layer, "Cargando...");
+
+  // Enviar mensaje al JS para que haga el fetch
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  
+  // Añadimos una llave dummy para avisar al JS que empiece
+  dict_write_uint8(iter, MESSAGE_KEY_FETCH_COMMAND, 1);
+  
+  app_message_outbox_send();
 }
 
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
-static void prv_window_load(Window *window) {
+// --- UI Y CICLO DE VIDA ---
+
+static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  s_status_layer = text_layer_create(GRect(5, 50, bounds.size.w - 10, 80));
+  text_layer_set_background_color(s_status_layer, GColorClear);
+  text_layer_set_text(s_status_layer, "Pulsa SELECT para consultar API");
+  text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_status_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(s_status_layer));
 }
 
-static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_status_layer);
 }
 
-static void prv_init(void) {
-  s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
+static void init() {
+  s_main_window = window_create();
+  window_set_click_config_provider(s_main_window, click_config_provider);
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
   });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+  window_stack_push(s_main_window, true);
+
+  // REGISTRO DE APP MESSAGE (Importante)
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  
+  // Abrir canales de comunicación (Tamaño de buffer entrada/salida)
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
 }
 
-static void prv_deinit(void) {
-  window_destroy(s_window);
+static void deinit() {
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
+  init();
   app_event_loop();
-  prv_deinit();
+  deinit();
 }
--- Anicorn/src/pkjs/index.js
+++ Anicorn/src/pkjs/index.js
@@ -1,5 +1,40 @@
-Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
-    }
-);
+// Escuchar cuando el JS está listo
+Pebble.addEventListener('ready', function(e) {
+  console.log('PebbleKit JS listo!');
+});
+
+// Escuchar mensajes desde el reloj (C)
+Pebble.addEventListener('appmessage', function(e) {
+  var dict = e.payload;
+  console.log('Mensaje recibido en JS: ' + JSON.stringify(dict));
+
+  if (dict['FETCH_COMMAND']) {
+    fetchData();
+  }
+});
+
+function fetchData() {
+  // Ejemplo con una API pública (JSONPlaceholder)
+  var url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    // JSON recibido del API
+    var json = JSON.parse(this.responseText);
+    
+    // Construimos el diccionario para enviar al reloj
+    // Usamos el nombre de la llave definido en package.json
+    var dictionary = {
+      'DATA_KEY': json.title // Enviamos el título de la tarea
+    };
+
+    // Enviar al Pebble
+    Pebble.sendAppMessage(dictionary, function(e) {
+      console.log('Info enviada al reloj con éxito');
+    }, function(e) {
+      console.log('Error enviando info al reloj');
+    });
+  };
+  xhr.open('GET', url);
+  xhr.send();
+}
